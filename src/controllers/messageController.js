c17ea36e2c71e7b0d9801aafaf9af776342dc720
quangtrinhhh <trinhngocquanggg@gmail.com>
2024-11-05 00:35:41 +0700

    api-group

--- src/controllers/messageController.js
+++ src/controllers/messageController.js
@@ -20,7 +20,26 @@ const getPrivateMessages = async (req, res) => {
     res.status(500).json(errorResponse(error.message));
   }
 };
+const getGroupMessages = async (req, res) => {
+  const userId = req.userId; // Lấy từ middleware
+
+  const groupId = req.params.groupId; // Lấy chatId từ URL
+
+  try {
+    const messages = await messageService.getMessagesGroup(userId, groupId);
+    console.log("Messages: ", messages); // Cải thiện ghi log
+
+    // Trả về phản hồi thành công
+    res.status(200).json(successResponse(messages));
+  } catch (error) {
+    console.error("Error fetching messages:", error); // Ghi log lỗi
+
+    // Trả về phản hồi lỗi
+    res.status(500).json(errorResponse(error.message));
+  }
+};
 
 module.exports = {
   getPrivateMessages,
+  getGroupMessages,
 };
